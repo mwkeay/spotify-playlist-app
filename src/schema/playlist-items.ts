a0f88b53cbb56d72c722867c09ca95f53135a97f
Michael Keay <mwkeay@gmail.com>
2025-10-03 17:41:49 +0100

    feat: add support for unions and arrays to zodSchemaToSpotifyFields function

--- src/schema/playlist-items.ts
+++ src/schema/playlist-items.ts
@@ -1,26 +1,43 @@
 import * as z from "zod";
 import zodSchemaToSpotifyFields from "./zodSchemaToSpotifyFields";
 
-const PlaylistItemsSchema = z.object({
+const PlaylistTrackSchema = z.object({
     album: z.object({
         name: z.string(),
+        images: z.array(z.object({
+            url: z.string(),
+            height: z.int(),
+            width: z.int(),
+        })),
     }),
+    artists: z.array(z.object({
+        name: z.string(),
+    })),
+    duration_ms: z.int(),
+    name: z.string(),
+    type: z.literal("track"),
+})
+
+const PlaylistEpisodeSchema = z.object({
+    duration_ms: z.int(),
+    images: z.array(z.object({
+        url: z.string(),
+        height: z.int(),
+        width: z.int(),
+    })),
+    name: z.string(),
+    show: z.object({
+        name: z.string(),
+    }),
+    type: z.literal("episode"),
+})
+
+const PlaylistItemsSchema = z.object({
     items: z.array(z.object({
-        track: z.object({
-            album: z.object({
-                name: z.string(),
-                images: z.array(z.object({
-                    url: z.object(),
-                    height: z.int(),
-                    width: z.int(),
-                })),
-            }),
-            artists: z.array(z.object({
-                name: z.string(),
-            })),
-            duration_ms: z.int(),
-            name: z.string(),
-        }),
+        track: z.union([
+            PlaylistTrackSchema,
+            PlaylistEpisodeSchema
+        ])
     })),
     total: z.int(),
 });
--- src/schema/zodSchemaToSpotifyFields.ts
+++ src/schema/zodSchemaToSpotifyFields.ts
@@ -2,9 +2,25 @@ import * as z from "zod";
 
 export default function zodSchemaToSpotifyFields(schema: z.ZodObject): string {
     return Object.entries(schema.shape)
-        .map(([key, value]) => value instanceof z.ZodObject
-            ? `${key}(${zodSchemaToSpotifyFields(value)})`
-            : key
-        )
+        .map(([key, value]) => {
+            if (value instanceof z.ZodObject) {
+                return `${key}(${zodSchemaToSpotifyFields(value)})`;
+            }
+            else if (value instanceof z.ZodArray && value.element instanceof z.ZodObject) {
+                return `${key}(${zodSchemaToSpotifyFields(value.element)})`;
+            }
+            else if (value instanceof z.ZodUnion) {
+                return `${key}(${
+                    value.def.options
+                        .filter(opt => opt instanceof z.ZodObject)
+                        .map(opt => zodSchemaToSpotifyFields(opt))
+                        .join(",")
+                })`
+            }
+            else {
+                return key
+            }
+        })
         .join(",");
 }
+
